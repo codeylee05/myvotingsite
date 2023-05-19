2ecb36833b927c750b0176aa46361867cfc6908f
codeylee05 <126554127+codeylee05@users.noreply.github.com>
2023-05-19 16:04:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+//index.html navigation buttons
+ var buttons = document.querySelectorAll(".navs");
+ for (var i = 0; i < buttons.length; i++) {
+   buttons[i].addEventListener("click", function() {
+     var url = this.getAttribute("data-url");
+     window.location.href = url;
+   });
+ };
+//reg.html button to go back home
+var button = document.getElementById("returnnav");
+button.addEventListener("click", function() {
+  window.location.href = "index.html";
+});
+//campaigns.html buttons to access pass.html (default)
+var button = document.getElementById("access-cand1");
+button.addEventListener("click", function() {
+  window.location.href = "pass.html";
+});
+
+var button = document.getElementById("access-cand2");
+button.addEventListener("click", function() {
+  window.location.href = "pass.html";
+});
